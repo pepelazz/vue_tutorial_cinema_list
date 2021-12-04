b110db0e12bca5413cb96fff0fb459274f5b252c
pepelazz <pepelazz00@gmail.com>
2021-12-04 22:03:24 +0300

    init

---
+++ cinema.js
@@ -0,0 +1,674 @@
+cinemaData = [
+ {
+  "id": 1,
+  "title": "Зеленая миля",
+  "title_original": "The Green Mile",
+  "year": 1999,
+  "genre": [
+   "фэнтези",
+   "драма",
+   "криминал",
+   "детектив"
+  ],
+  "poster": "the_green_mile.webp",
+  "rating": 9.1
+ },
+ {
+  "id": 2,
+  "title": "Побег из Шоушенка",
+  "title_original": "The Shawshank Redemption",
+  "year": 1994,
+  "genre": [
+   "драма"
+  ],
+  "poster": "the_shawshank_redemption.webp",
+  "rating": 9.1
+ },
+ {
+  "id": 3,
+  "title": "Властелин колец: Возвращение короля",
+  "title_original": "The Lord of the Rings: The Return of the King",
+  "year": 2003,
+  "genre": [
+   "фэнтези",
+   "приключения",
+   "драма"
+  ],
+  "poster": "the_lord_of_the_rings_the_return_of_the_king.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 4,
+  "title": "Властелин колец: Две крепости",
+  "title_original": "The Lord of the Rings: The Two Towers",
+  "year": 2002,
+  "genre": [
+   "фэнтези",
+   "приключения",
+   "драма"
+  ],
+  "poster": "the_lord_of_the_rings_the_two_towers.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 5,
+  "title": "Властелин колец: Братство Кольца",
+  "title_original": "The Lord of the Rings: The Fellowship of the Ring",
+  "year": 2001,
+  "genre": [
+   "фэнтези",
+   "приключения",
+   "драма"
+  ],
+  "poster": "the_lord_of_the_rings_the_fellowship_of_the_ring.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 6,
+  "title": "Форрест Гамп",
+  "title_original": "Forrest Gump",
+  "year": 1994,
+  "genre": [
+   "драма",
+   "комедия",
+   "мелодрама",
+   "история",
+   "военный"
+  ],
+  "poster": "forrest_gump.webp",
+  "rating": 8.9
+ },
+ {
+  "id": 7,
+  "title": "Иван Васильевич меняет профессию",
+  "title_original": "",
+  "year": 1973,
+  "genre": [
+   "комедия",
+   "фантастика",
+   "приключения"
+  ],
+  "poster": "ivan_vasilievish.webp",
+  "rating": 8.8
+ },
+ {
+  "id": 8,
+  "title": "Король Лев",
+  "title_original": "The Lion King",
+  "year": 1994,
+  "genre": [
+   "мультфильм",
+   "мюзикл",
+   "драма",
+   "приключения",
+   "семейный"
+  ],
+  "poster": "the_lion_king.webp",
+  "rating": 8.8
+ },
+ {
+  "id": 9,
+  "title": "Интерстеллар",
+  "title_original": "Interstellar",
+  "year": 2014,
+  "genre": [
+   "фантастика",
+   "драма",
+   "приключения"
+  ],
+  "poster": "interstellar.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 10,
+  "title": "Тайна Коко",
+  "title_original": "Coco",
+  "year": 2017,
+  "genre": [
+   "мультфильм",
+   "фэнтези",
+   "детектив",
+   "приключения",
+   "семейный",
+   "музыка"
+  ],
+  "poster": "coco.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 11,
+  "title": "Криминальное чтиво",
+  "title_original": "Pulp Fiction",
+  "year": 1994,
+  "genre": [
+   "криминал",
+   "драма"
+  ],
+  "poster": "pulp_fiction.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 12,
+  "title": "ВАЛЛ·И",
+  "title_original": "WALL·E",
+  "year": 2008,
+  "genre": [
+   "мультфильм",
+   "фантастика",
+   "приключения",
+   "семейный"
+  ],
+  "poster": "wall·e.webp",
+  "rating": 8.3
+ },
+ {
+  "id": 13,
+  "title": "Карты, деньги, два ствола",
+  "title_original": "Lock, Stock and Two Smoking Barrels",
+  "year": 0,
+  "genre": [
+   "боевик",
+   "комедия",
+   "криминал"
+  ],
+  "poster": "lock,_stock_and_two_smoking_barrels.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 14,
+  "title": "Матрица",
+  "title_original": "The Matrix",
+  "year": 1999,
+  "genre": [
+   "фантастика",
+   "боевик"
+  ],
+  "poster": "the_matrix.webp",
+  "rating": 8.5
+ },
+ {
+  "id": 15,
+  "title": "1+1",
+  "title_original": "Intouchables",
+  "year": 0,
+  "genre": [
+   "драма",
+   "комедия",
+   "биография"
+  ],
+  "poster": "intouchables.webp",
+  "rating": 8.8
+ },
+ {
+  "id": 16,
+  "title": "Начало",
+  "title_original": "Inception",
+  "year": 2010,
+  "genre": [
+   "фантастика",
+   "боевик",
+   "триллер",
+   "драма",
+   "детектив"
+  ],
+  "poster": "inception.webp",
+  "rating": 8.7
+ },
+ {
+  "id": 17,
+  "title": "Клаус",
+  "title_original": "Klaus",
+  "year": 2019,
+  "genre": [
+   "мультфильм",
+   "семейный",
+   "комедия",
+   "приключения"
+  ],
+  "poster": "klaus.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 18,
+  "title": "Список Шиндлера",
+  "title_original": "Schindler's List",
+  "year": 1993,
+  "genre": [
+   "драма",
+   "биография",
+   "история",
+   "военный"
+  ],
+  "poster": "schindler's_list.webp",
+  "rating": 8.8
+ },
+ {
+  "id": 19,
+  "title": "Унесённые призраками",
+  "title_original": "Sen to Chihiro no kamikakushi",
+  "year": 2001,
+  "genre": [
+   "аниме",
+   "мультфильм",
+   "фэнтези",
+   "приключения",
+   "семейный"
+  ],
+  "poster": "sen_to_chihiro_no_kamikakushi.webp",
+  "rating": 8.4
+ },
+ {
+  "id": 20,
+  "title": "Назад в будущее",
+  "title_original": "Back to the Future",
+  "year": 1985,
+  "genre": [
+   "фантастика",
+   "комедия",
+   "приключения"
+  ],
+  "poster": "back_to_the_future.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 21,
+  "title": "Джентльмены удачи",
+  "title_original": "Джентльмены удачи",
+  "year": 1971,
+  "genre": [
+   "комедия",
+   "драма",
+   "криминал",
+   "детектив"
+  ],
+  "poster": "gentelmen_udachi.webp",
+  "rating": 8.5
+ },
+ {
+  "id": 22,
+  "title": "Приключения Шерлока Холмса и доктора Ватсона: Собака Баскервилей",
+  "title_original": "Приключения Шерлока Холмса и доктора Ватсона: Собака Баскервилей",
+  "year": 1981,
+  "genre": [
+   "детектив",
+   "криминал",
+   "приключения"
+  ],
+  "poster": "sobaka_baskervilly.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 23,
+  "title": "Операция «Ы» и другие приключения Шурика",
+  "title_original": "",
+  "year": 1965,
+  "genre": [
+   "комедия",
+   "мелодрама",
+   "криминал"
+  ],
+  "poster": "operation_y.webp",
+  "rating": 8.7
+ },
+ {
+  "id": 24,
+  "title": "Пираты Карибского моря: Проклятие Черной жемчужины",
+  "title_original": "Pirates of the Caribbean: The Curse of the Black Pearl",
+  "year": 2003,
+  "genre": [
+   "фэнтези",
+   "боевик",
+   "приключения"
+  ],
+  "poster": "pirates_of_the_caribbean_the_curse_of_the_black_pearl.webp",
+  "rating": 8.3
+ },
+ {
+  "id": 25,
+  "title": "Москва слезам не верит",
+  "title_original": "",
+  "year": 1979,
+  "genre": [
+   "мелодрама",
+   "драма",
+   "комедия"
+  ],
+  "poster": "moskva_slezam_ne_verit.webp",
+  "rating": 8.4
+ },
+ {
+  "id": 26,
+  "title": "В бой идут одни «старики»",
+  "title_original": "",
+  "year": 1973,
+  "genre": [
+   "военный",
+   "драма",
+   "комедия"
+  ],
+  "poster": "v_biy_idut_odni_stariki.webp",
+  "rating": 8.7
+ },
+ {
+  "id": 27,
+  "title": "Остров проклятых",
+  "title_original": "Shutter Island",
+  "year": 2009,
+  "genre": [
+   "триллер",
+   "детектив",
+   "драма"
+  ],
+  "poster": "shutter_island.webp",
+  "rating": 8.5
+ },
+ {
+  "id": 28,
+  "title": "Большой куш",
+  "title_original": "Snatch",
+  "year": 2000,
+  "genre": [
+   "криминал",
+   "комедия",
+   "боевик"
+  ],
+  "poster": "snatch.webp",
+  "rating": 8.5
+ },
+ {
+  "id": 29,
+  "title": "Джентльмены",
+  "title_original": "The Gentlemen",
+  "year": 2019,
+  "genre": [
+   "криминал",
+   "комедия",
+   "боевик"
+  ],
+  "poster": "the_gentlemen.webp",
+  "rating": 8.5
+ },
+ {
+  "id": 30,
+  "title": "Бойцовский клуб",
+  "title_original": "Fight Club",
+  "year": 1999,
+  "genre": [
+   "триллер",
+   "драма",
+   "криминал"
+  ],
+  "poster": "fight_club.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 31,
+  "title": "Достучаться до небес",
+  "title_original": "Knockin' on Heaven's Door",
+  "year": 0,
+  "genre": [
+   "драма",
+   "комедия",
+   "криминал"
+  ],
+  "poster": "knockin_on_heavens_door.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 32,
+  "title": "Шрэк",
+  "title_original": "Shrek",
+  "year": 2001,
+  "genre": [
+   "мультфильм",
+   "фэнтези",
+   "комедия",
+   "приключения",
+   "семейный"
+  ],
+  "poster": "shrek.webp",
+  "rating": 8
+ },
+ {
+  "id": 33,
+  "title": "...А зори здесь тихие",
+  "title_original": "",
+  "year": 1972,
+  "genre": [
+   "драма",
+   "военный",
+   "история"
+  ],
+  "poster": "a_zori_zdec_tihie.webp",
+  "rating": 8.5
+ },
+ {
+  "id": 34,
+  "title": "Брат",
+  "title_original": "",
+  "year": 1997,
+  "genre": [
+   "драма",
+   "криминал",
+   "боевик"
+  ],
+  "poster": "brat.webp",
+  "rating": 8.2
+ },
+ {
+  "id": 35,
+  "title": "Невидимая сторона",
+  "title_original": "The Blind Side",
+  "year": 2009,
+  "genre": [
+   "драма",
+   "спорт",
+   "биография"
+  ],
+  "poster": "the_blind_side.webp",
+  "rating": 8.2
+ },
+ {
+  "id": 36,
+  "title": "Душа",
+  "title_original": "Soul",
+  "year": 2020,
+  "genre": [
+   "мультфильм",
+   "фэнтези",
+   "драма",
+   "комедия",
+   "приключения",
+   "семейный",
+   "музыка"
+  ],
+  "poster": "soul.webp",
+  "rating": 8.4
+ },
+ {
+  "id": 37,
+  "title": "Остров сокровищ",
+  "title_original": "",
+  "year": 1988,
+  "genre": [
+   "мультфильм",
+   "приключения",
+   "мюзикл",
+   "комедия"
+  ],
+  "poster": "ostrov_sokrovich.webp",
+  "rating": 8.2
+ },
+ {
+  "id": 38,
+  "title": "Один дома",
+  "title_original": "Home Alone",
+  "year": 1990,
+  "genre": [
+   "комедия",
+   "семейный"
+  ],
+  "poster": "home_alone.webp",
+  "rating": 8.2
+ },
+ {
+  "id": 39,
+  "title": "Темный рыцарь",
+  "title_original": "The Dark Knight",
+  "year": 2008,
+  "genre": [
+   "фантастика",
+   "боевик",
+   "триллер",
+   "криминал",
+   "драма"
+  ],
+  "poster": "the_dark_knight.webp",
+  "rating": 8.5
+ },
+ {
+  "id": 40,
+  "title": "12 разгневанных мужчин",
+  "title_original": "12 Angry Men",
+  "year": 1956,
+  "genre": [
+   "драма",
+   "детектив",
+   "криминал"
+  ],
+  "poster": "12_angry_men.webp",
+  "rating": 8.5
+ },
+ {
+  "id": 41,
+  "title": "Престиж",
+  "title_original": "The Prestige",
+  "year": 2006,
+  "genre": [
+   "триллер",
+   "фантастика",
+   "драма",
+   "детектив"
+  ],
+  "poster": "the_prestige.webp",
+  "rating": 8.5
+ },
+ {
+  "id": 42,
+  "title": "Гладиатор",
+  "title_original": "Gladiator",
+  "year": 2000,
+  "genre": [
+   "история",
+   "боевик",
+   "драма",
+   "приключения"
+  ],
+  "poster": "gladiator.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 43,
+  "title": "Терминатор 2: Судный день",
+  "title_original": "Terminator 2: Judgment Day",
+  "year": 1991,
+  "genre": [
+   "фантастика",
+   "боевик",
+   "триллер"
+  ],
+  "poster": "terminator_2_judgment_day.webp",
+  "rating": 8.3
+ },
+ {
+  "id": 44,
+  "title": "Любовь и голуби",
+  "title_original": "",
+  "year": 1984,
+  "genre": [
+   "мелодрама",
+   "комедия"
+  ],
+  "poster": "lubov_i_golubi.webp",
+  "rating": 8.3
+ },
+ {
+  "id": 45,
+  "title": "Игры разума",
+  "title_original": "A Beautiful Mind",
+  "year": 2001,
+  "genre": [
+   "драма",
+   "биография",
+   "мелодрама"
+  ],
+  "poster": "a_beautiful_mind.webp",
+  "rating": 8.6
+ },
+ {
+  "id": 46,
+  "title": "Одержимость",
+  "title_original": "Whiplash",
+  "year": 2013,
+  "genre": [
+   "драма",
+   "музыка"
+  ],
+  "poster": "whiplash.webp",
+  "rating": 8.3
+ },
+ {
+  "id": 47,
+  "title": "Собачье сердце",
+  "title_original": "",
+  "year": 1988,
+  "genre": [
+   "фантастика",
+   "драма",
+   "комедия"
+  ],
+  "poster": "sobachie_serdce.webp",
+  "rating": 8.3
+ },
+ {
+  "id": 48,
+  "title": "Леон",
+  "title_original": "Léon",
+  "year": 0,
+  "genre": [
+   "боевик",
+   "триллер",
+   "драма",
+   "криминал"
+  ],
+  "poster": "léon.webp",
+  "rating": 8.7
+ },
+ {
+  "id": 49,
+  "title": "Брат 2",
+  "title_original": "",
+  "year": 2000,
+  "genre": [
+   "боевик",
+   "криминал"
+  ],
+  "poster": "brat_2.webp",
+  "rating": 8.1
+ },
+ {
+  "id": 50,
+  "title": "Твоё имя",
+  "title_original": "Kimi no na wa",
+  "year": 2016,
+  "genre": [
+   "аниме",
+   "мультфильм",
+   "фэнтези",
+   "драма",
+   "мелодрама"
+  ],
+  "poster": "kimi_no_na_wa.webp",
+  "rating": 8.3
+ }
+]
